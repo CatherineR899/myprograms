9813a32e2cb60352d0a6d219961e1d5864975fcc
CatherineR899 <catherine.ren2334@gmail.com>
2026-02-06 11:48:00 -0500

    hey mom and dad, i made a program :)

--- first.py
+++ first.py
@@ -1,2 +1,31 @@
-# This is a comment, the interpreter ignores this line
-print("Sup cuh?")
+    # variables names should be meaningful and cannot start with numbers or uppercase letters 
+    # variable data types do not need to be specified 
+    # variable data types can be: 
+        #   bool: true or false 
+        #   int: integer 
+        #   float: positive or negative decimals 
+        #   str: alpha, numeric, & special symbols  
+        #   list: holds many data types, similar to Arraylists but does not only house one data type 
+        #   dict: dictionary 
+
+    # equal sign, "=", is an assignment operator, assigns a value to a variable 
+    # input: a built in function that grabs and stores string input from terminal to the related variable, similar to sc.nextLine() 
+    # end: prints an ending string, when it is not specified it prints a next line 
+    # sep: prints a string that seperates variables when printed, when not specific it prints a space 
+
+# message = "Hello, welcome to my first legit program, "
+# name = input("What is your name? ")
+# print(message,name, end=" ur mom", sep=" ")
+
+    #ctrl + / : makes the lines highlighted messages, types # infront of the lines 
+    #ctrl + z : undo 
+
+    #input always reads inputs as str 
+    #use int to change value into an int 
+
+# num1 = 6
+# num2 = 7
+# print(num1 + num2)
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print(num1 + num2)
